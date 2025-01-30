ef341336c7c00c2abc85c5df81865323f042b75f
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-29 22:33:36 -0600

    Variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+
+const nombre = 'Mauricio';
+const apellido = 'Goiz';
+
+let valorDado = 6;
+valorDado = 4
+
+console.log( nombre, apellido, valorDado );
+
+if ( true ) {
+    const nombre = 'Enrique';
+    console.log( nombre );
+}
+
+console.log( valorDado );
