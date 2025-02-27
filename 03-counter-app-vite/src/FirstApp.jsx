4ba51b84dff07bfd09d445f0cc5126cda7e0ed51
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-02-26 20:23:06 -0600

    Impresion de variables en HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,21 @@
 
+// const newMessage = {
+//     message: 'Hola Mundo',
+//     title: 'Mauricio',
+// };
+
+const getName = ( name ) => {
+    return `Hola ${ name }`;
+}
+
 export const FirstApp = () => {
 
     return (
         <>
-            <h1>Mauricio</h1>
+            <h1>Hola mundo</h1>
+            {/* <code>{ JSON.stringify( newMessage ) }</code> */}
             <p>Soy un subtitulo</p>
+            <p>{ getName( 'Mauri' ) }</p>
         </>        
     )        
 }
