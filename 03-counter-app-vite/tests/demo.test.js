1b654cd0411bfa1651f1b6472d59df262d1daed7
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-05-02 22:50:29 -0600

    Jest - Expect - toBe

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,8 +1,21 @@
 
-test('Este prueba no debe de fallar', () => {
+describe('Pruebas en <DemoComponent>', () => {
 
-    if ( 0 === 1 ) {
-        throw new Error('No puede dividir entre 0');
-    }
+    test('Este prueba no debe de fallar', () => {
+
+        // 1. Inicialización
+        const message1 = 'Hola Mundo';
+    
+    
+        // 2. Estímulo
+        const message2 = message1.trim();
+        
+        // 3. Observar el comportamiento... esperado
+        expect(message1).toBe( message2 );
+    
+    })
+    
 
 })
+
+
