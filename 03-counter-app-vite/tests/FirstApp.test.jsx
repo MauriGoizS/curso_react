c09f2875709b36d273ba1f2a16b061d2589c23b6
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-05-17 02:22:51 -0600

    getByTestId y otras props

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -2,22 +2,39 @@ const { render } = require("@testing-library/react")
 const { FirstApp } = require("../src/FirstApp")
 
 describe('Tests in <FirstApp />', () => {
-    test('Must match the snapshot', () => {
+    
+    // test('Must match the snapshot', () => {
         
-        const title = 'Hi, I am Goku'
-        const { container } = render( <FirstApp title={ title }/>);
+    //     const title = 'Hi, I am Goku'
+    //     const { container } = render( <FirstApp title={ title }/>);
 
-        expect( container ).toMatchSnapshot();
-    });
+    //     expect( container ).toMatchSnapshot();
+    // });
     
     test('Must display the title in a h1', () => {
         
         const title = 'Hi, I am Goku'
-        const { container, getByText } = render( <FirstApp title={ title }/>);
+        const { container, getByText, getByTestId } = render( <FirstApp title={ title }/>);
         expect( getByText(title) ).toBeTruthy();
 
-        const h1 = container.querySelector('h1');
-        expect(h1.innerHTML).toContain( title );
+        // const h1 = container.querySelector('h1');
+        // expect(h1.innerHTML).toContain( title );
+        expect( getByTestId('test-title').innerHTML ).toContain( title );
+
+    })
+    
+    test('Must match the subtitle send via props', () => {
+    
+        const title = 'Hi, I´m Goku';
+        const subTitle = 'I´m subtitle';
+        const { getAllByText } = render(
+            <FirstApp 
+            title={ title }
+            subTitle= { subTitle }
+            />
+        );
+        
+        expect( getAllByText(subTitle).length ).toBe(2);
 
     })
     
