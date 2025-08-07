aa2ef7ddc8aaa37c35390b63b186289859260437
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-08-07 16:10:21 -0600

    Creando una lista de categorias

--- 04-git-expert-app/src/main.jsx
+++ 04-git-expert-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { GifExpertApp } from './GifExpert';
+import { GifExpertApp } from './GifExpertApp';
 
 import './styles.css';
 
