bc970c06f09f589815fbcd0b1347d08956a89a91
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-08-11 22:38:31 -0600

    useEffect

--- 04-git-expert-app/src/components/GifGrid.jsx
+++ 04-git-expert-app/src/components/GifGrid.jsx
@@ -1,13 +1,22 @@
+import { useState, useEffect } from "react";
 import { getGifts } from "../helpers/getGifs";
 
 const GifGrid = ({ category }) => {
     
-    getGifts( category );
+    const [counter, setCounter] = useState(10);
 
+    useEffect( () => {
+
+        getGifts( category );
+
+    }, [])
+    
     return (
         <>
-            <h3> { category } </h3>  
-            
+            <h3> { category }</h3>
+
+            <h5> { counter } </h5>  
+            <button onClick={ () => setCounter( counter + 1 ) } > +1</button>
         </>
     )
 }
