c05dd35ca3d98ada6ce007b8ff1495165d13305e
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-08-22 20:26:53 -0600

    Variables de entorno

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.actions.ts
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.actions.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
 import type { GiphyResponse } from "../interfaces/giphy.response";
 import type { Gif } from "../interfaces/gif.interface";
+import { giphyApi } from "../api/giphy.api";
 
 export const getGifsByQuery = async (  query: string): Promise<Gif[]> => {
 
-    const response = await axios.get<GiphyResponse>('https://api.giphy.com/v1/gifs/search', {
+    const response = await giphyApi<GiphyResponse>(
+        '/search',
+        {
         params: {
             q: query,
-            limit:10 ,
-            lang: 'es',
-            api_key: 'qDD4kPr1TMkHtmnMGo4BiaBD4An9KHOi'
+            limit:10,
         }
     });
 
